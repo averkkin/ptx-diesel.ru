9f71b6593348611b92226da5f67d5f23f413a7bf
averkkin <info@averkkin.ru>
2025-11-27 23:40:46 +0300

    Added new components

--- footer.php
+++ footer.php
@@ -11,22 +11,10 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ptz-diesel' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'ptz-diesel' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ptz-diesel' ), 'ptz-diesel', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
+	<footer>
+
 	</footer><!-- #colophon -->
-</div><!-- #page -->
+</main><!-- #page -->
 
 <?php wp_footer(); ?>
 
